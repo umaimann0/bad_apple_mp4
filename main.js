2987f361472bf689f30d7797b7033adb2b2711d4
umaimann0 <umaimann01@gmail.com>
2026-02-01 12:36:44 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+//[HD60FPS] Bad Apple!! [Shadow Art PV] - YoshiFan (avrilloosing) (720p, h264).mp4
+const video = document.getElementById("video");
+video.src = "./[HD60FPS] Bad Apple!! [Shadow Art PV] - YoshiFan (avrilloosing) (720p, h264).mp4";
